2f80b46ea63ab3f12bcfac553a328a85c301dd5d
Alder Fulton <fultona5@msu.edu>
2023-02-27 15:23:26 -0500

    Modified LoadImage to fit our new structure.

--- src/LoadImage.py
+++ src/LoadImage.py
@@ -1,30 +1,26 @@
-# Parameters. Will use ArgParser later on.
-IMAGE_FILE = "../data/Kzoo_Office_v5_orthomosaic.tif"
-DIVIDE_X = 25
-DIVIDE_Y = 25
-
-
-# Code below
 import numpy as np
 import skimage
 from skimage import io
 
-# Read in the image file.
-img = io.imread(IMAGE_FILE) 
+def loadImage(IMAGE_FILE, DIVIDE_X, DIVIDE_Y):
+    # Read in the image file.
+    img = io.imread(IMAGE_FILE) 
 
-# Create the framework for cutting the image.
-maxx = img.shape[0]
-maxy = img.shape[1]
-x_divide = [round(i) for i in np.linspace(0,maxx, DIVIDE_X + 1)]
-y_divide = [round(i) for i in np.linspace(0,maxy, DIVIDE_Y + 1)]
-x=0
-y=0
+    # Create the framework for cutting the image.
+    maxx = img.shape[0]
+    maxy = img.shape[1]
+    x_divide = [round(i) for i in np.linspace(0,maxx, DIVIDE_X + 1)]
+    y_divide = [round(i) for i in np.linspace(0,maxy, DIVIDE_Y + 1)]
+    x=0
+    y=0
 
-# Create the dictionary to hold the cut images.
-# We may want to add metadata later on
-cut_img_dict = {}
+    # Create the dictionary to hold the cut images.
+    # We may want to add metadata later on
+    cut_img_dict = {}
 
-# Actually cut and add the cut images into the dictionary.
-for x in range(len(x_divide) - 1):
-    for y in range(len(y_divide) - 1):
-        cut_img_dict[(x,y)] = img[x_divide[x]:x_divide[x+1],y_divide[y]:y_divide[y+1]]
+    # Actually cut and add the cut images into the dictionary.
+    for x in range(len(x_divide) - 1):
+        for y in range(len(y_divide) - 1):
+            cut_img_dict[(x,y)] = img[x_divide[x]:x_divide[x+1],y_divide[y]:y_divide[y+1]]
+
+    return cut_img_dict
