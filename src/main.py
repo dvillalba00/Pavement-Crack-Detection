3283dfbc15a9d5b4cf9a07e9fdb6f7ecf77aa084
parksjr5 <jruthsmi@umich.edu>
2023-02-27 15:54:26 -0500

    comments added to main.py

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@ from SaveCutImages import saveCutImages
 from ThresholdCutImages import thresholdCutImages
 from SaveThresholdedImages import saveThresholdedImages
 
+# import image
 IMAGE_FILE = "../data/Kzoo_Office_v5_orthomosaic.tif"
+# horizontal and vertical size cuts to make
 DIVIDE_X = 25
 DIVIDE_Y = 25
 
+# paths to cut image, thresholded cut images
 paths = [
     "../data",
     "../results",
@@ -16,10 +19,11 @@ paths = [
     "../results/thresholded_cut_images"
 ]
 
+# user chosen function for thresholding
 thresholding_function = threshold_multiotsu
 
-createPaths(paths)
-cut_img_dict = loadImage(IMAGE_FILE, DIVIDE_X, DIVIDE_Y)
-saveCutImages(cut_img_dict)
-thresholded_dict = thresholdCutImages(cut_img_dict, thresholding_function)
-saveThresholdedImages(thresholded_dict)
+createPaths(paths) # create path for image
+cut_img_dict = loadImage(IMAGE_FILE, DIVIDE_X, DIVIDE_Y) # image, amount of cuts
+saveCutImages(cut_img_dict) # saving cut image to later threshold
+thresholded_dict = thresholdCutImages(cut_img_dict, thresholding_function) # run cut images through threshold function
+saveThresholdedImages(thresholded_dict) # save threshold images
