bb3b7034242c69cba17626fa239b4c2c2db14f86
Alder Fulton <fultona5@msu.edu>
2023-02-27 15:28:51 -0500

    Updated ThresholdCutImages to be in line with the updated standard as well as adding the functionality to pass in the thresholding function as an argument.

--- src/ThresholdCutImages.py
+++ src/ThresholdCutImages.py
@@ -1,13 +1,14 @@
-from LoadImage import cut_img_dict
 import numpy as np
 from skimage.filters import threshold_multiotsu
 
-thresholded_dict = {}
-for key in cut_img_dict:
-    if len(np.unique(cut_img_dict[key])) < 3:
-        thresholded_dict[key] = None
-    else:                         
-        image = np.dot(cut_img_dict[key][...,:3], [0.299, 0.587, 0.114])
-        thresholds = threshold_multiotsu(image)
-        regions = np.digitize(image, bins=thresholds)
-        thresholded_dict[key] = regions == 0
+def thresholdCutImages(cut_img_dict, thresholding_function)
+    thresholded_dict = {}
+    for key in cut_img_dict:
+        if len(np.unique(cut_img_dict[key])) < 3:
+            thresholded_dict[key] = None
+        else:                         
+            image = np.dot(cut_img_dict[key][...,:3], [0.299, 0.587, 0.114])
+            thresholds = thresholding_function(image)
+            regions = np.digitize(image, bins=thresholds)
+            thresholded_dict[key] = regions == 0
+    return thresholded_dict
