94284faaa83592e8cd3716387da3c6c0925f8f1c
Alder Fulton <fultona5@msu.edu>
2023-03-13 16:08:18 -0400

    wip

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ thresholding_function = threshold_multiotsu
 
 createPaths(paths, dir_path) # create path for image
 cut_img_dict = loadImage(IMAGE_FILE, DIVIDE_X, DIVIDE_Y) # image, amount of cuts
-saveCutImages(cut_img_dict, dir_path) # saving cut image to later threshold
+# saveCutImages(cut_img_dict, dir_path) # saving cut image to later threshold
 thresholded_dict = thresholdCutImages(cut_img_dict, thresholding_function) # run cut images through threshold function
-saveThresholdedImages(thresholded_dict, dir_path) # save threshold images
+# saveThresholdedImages(thresholded_dict, dir_path) # save threshold images
+labeled_image_dict = loadLabeledImages(dir_path + '/data/ground_truth', dir_path + '/LabeledImages.txt')
+saveLabeledCutImages(labeled_image_dict)
+
