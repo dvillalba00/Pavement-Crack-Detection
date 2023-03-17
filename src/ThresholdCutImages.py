64f5e975b84f89f676fcc518ec22f651cbc98a18
Alder Fulton <fultona5@msu.edu>
2023-03-17 15:26:45 -0400

    Fixed thresholdCutImage functionality within the .py file and removed the function's definition from the Interactive file.

--- src/ThresholdCutImages.py
+++ src/ThresholdCutImages.py
@@ -4,10 +4,14 @@ def thresholdCutImages(cut_img_dict, thresholding_function):
     thresholded_dict = {}
     for key in cut_img_dict:
         if len(np.unique(cut_img_dict[key])) < 3:
-            thresholded_dict[key] = None
+            thresholded_dict[key] = None 
         else:                         
             image = np.dot(cut_img_dict[key][...,:3], [0.299, 0.587, 0.114])
             thresholds = thresholding_function(image)
             regions = np.digitize(image, bins=thresholds)
             thresholded_dict[key] = regions == 0
+        
+        if type(thresholded_dict[key]) == type(None):
+            thresholded_dict[key] = np.full(cut_img_dict[key].shape, False)
+    
     return thresholded_dict
