adec98d06601eac4eff48bee9f5c268a60320016
Alder Fulton <fultona5@msu.edu>
2023-03-22 14:52:43 -0400

    Removed the obselete files Argparser.py, SaveCutImages.py, and SaveThresholdedImages.py. The two saving functions already work with the save dictionary file, and the argparser is no longer needed because we are using an interactive notebook.

--- src/ArgParser.py
+++
@@ -1,8 +0,0 @@
-import argparse
-# argparser for CLI usage
-def parseArgs():
-  parser = argparse.ArgumentParser(description="Thresholds images to find cracks in pavement based on a tiff file image")
-  parser.add_argument('-x', type=int, help="sets the width value when cutting images", default=25)
-  parser.add_argument('-y', type=int, help="sets the height value when cutting images", default=25)
-  parser.add_argument('-f', '--file', required=True, dest="filename", help="name of the image you want to process")
-  return parser.parse_args()
