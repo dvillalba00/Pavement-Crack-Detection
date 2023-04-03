18d7924aec464839411aa759e7be6c39d5501ace
Alder Fulton <fultona5@msu.edu>
2023-04-03 15:43:29 -0400

    Added the code that saves the stitched image into the dictionary in order to work with it in the future. Also, it saves it inside the interactive notebook.

--- src/StitchImageDict.py
+++ src/StitchImageDict.py
@@ -1,15 +1,16 @@
 from skimage import io
 import numpy as np
+import os
 
 def stitchRows(img_dict, row, startx = 0, stopx = 24):
-  temp_row = img_dict[(row,0)].copy()
-  for x in range(startx + 1, stopx + 1):
-    temp_row = np.concatenate((temp_row, img_dict[(row, x)]), axis=1)
-  return temp_row
+    temp_row = img_dict[(row,0)].copy()
+    for x in range(startx + 1, stopx + 1):
+        temp_row = np.concatenate((temp_row, img_dict[(row, x)]), axis=1)
+    return temp_row
 
-def stitchImageDict(img_dict, dir_path, starty = 0, stopy = 24):
-  img = stitchRows(img_dict, 0)
-  for y in range(starty + 1, stopy + 1):
-    new_row = stitchRows(img_dict, y)
-    img = np.concatenate((img, new_row), axis=0)
-  io.imsave(dir_path + "/results/stitched_image/stitched_image.tif", img, check_contrast=False)
+def stitchImageDict(img_dict, starty = 0, stopy = 24):
+    img = stitchRows(img_dict, 0)
+    for y in range(starty + 1, stopy + 1):
+        new_row = stitchRows(img_dict, y)
+        img = np.concatenate((img, new_row), axis=0)
+    return img
